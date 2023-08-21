277bffcc3e944dbcef49f9946d6f08f040541a2e
Romina-Ramirez <romina.maite.rt@gmail.com>
2023-08-21 17:17:33 -0500

    Almacenamiento y Cámara

--- app/src/main/java/com/example/memoraid/ui/activities/DatosUsuarioActivity.kt
+++ app/src/main/java/com/example/memoraid/ui/activities/DatosUsuarioActivity.kt
@@ -1,5 +1,4 @@
 package com.example.memoraid.ui.activities
-
 import android.annotation.SuppressLint
 
 import android.content.Intent
@@ -10,6 +9,8 @@ import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.example.memoraid.R
@@ -32,6 +33,22 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+
+//camara y almacenamiento
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+import android.view.View
+import android.widget.Toast
+
 class DatosUsuarioActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDatosUsuarioBinding
@@ -43,12 +60,126 @@ class DatosUsuarioActivity : AppCompatActivity() {
     private var currentLocation: Location? = null
     private lateinit var locationSettingRequest: LocationSettingsRequest
 
+    private val CAMERA_PERMISSION_REQUEST = 100
+    private val STORAGE_PERMISSION_REQUEST = 101
+    private val TAKE_PICTURE_REQUEST = 102
+    private val PICK_IMAGE_REQUEST = 103
+
+    private lateinit var usuarioImg: ImageView
+    private lateinit var btnCambiarFoto: Button
+    private var selectedImageUri: Uri? = null // Variable para almacenar la URI de la imagen seleccionada o capturada
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         binding = ActivityDatosUsuarioBinding.inflate(layoutInflater)
         client = LocationServices.getSettingsClient(this)
         setContentView(binding.root)
+
+        // Inicialización de vistas
+        usuarioImg = findViewById(R.id.usuarioImg)
+        btnCambiarFoto = findViewById(R.id.btnCambiarFoto)
+
+        // Configuración del listener para el botón "Cambiar Foto de Perfil"
+        btnCambiarFoto.setOnClickListener {
+            val options = arrayOf("Tomar Foto", "Seleccionar de la Galería")
+            AlertDialog.Builder(this)
+                .setTitle("Elige una opción")
+                .setItems(options) { _, which ->
+                    when (which) {
+                        0 -> {
+                            // Opción: Tomar Foto
+                            if (ContextCompat.checkSelfPermission(
+                                    this,
+                                    Manifest.permission.CAMERA
+                                ) == PackageManager.PERMISSION_GRANTED
+                            ) {
+                                abrirCamara()
+                            } else {
+                                ActivityCompat.requestPermissions(
+                                    this,
+                                    arrayOf(Manifest.permission.CAMERA),
+                                    CAMERA_PERMISSION_REQUEST
+                                )
+                            }
+                        }
+                        1 -> {
+                            // Opción: Seleccionar de la Galería
+                            if (ContextCompat.checkSelfPermission(
+                                    this,
+                                    Manifest.permission.READ_EXTERNAL_STORAGE
+                                ) == PackageManager.PERMISSION_GRANTED
+                            ) {
+                                abrirGaleria()
+                            } else {
+                                ActivityCompat.requestPermissions(
+                                    this,
+                                    arrayOf(
+                                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                                    ),
+                                    STORAGE_PERMISSION_REQUEST
+                                )
+                            }
+                        }
+                    }
+                }
+                .show()
+        }
+    }
+
+    private fun abrirCamara() {
+        // Crear un intent para abrir la cámara y capturar una imagen
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(takePictureIntent, TAKE_PICTURE_REQUEST)
+    }
+
+    private fun abrirGaleria() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    // Manejo de la respuesta a las solicitudes de permisos
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST -> {
+                // Verificar si el permiso de cámara fue concedido
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Si el permiso fue concedido, abrir la cámara
+                    abrirCamara()
+                }
+            }
+            STORAGE_PERMISSION_REQUEST -> {
+                // Verificar si el permiso de almacenamiento fue concedido
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Si el permiso fue concedido, abrir la galería
+                    abrirGaleria()
+                }
+            }
+        }
+    }
+
+    // Manejo de la respuesta a las actividades de cámara y galería
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                TAKE_PICTURE_REQUEST -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    usuarioImg.setImageBitmap(imageBitmap)
+                }
+                PICK_IMAGE_REQUEST -> {
+                    val imageUri: Uri = data?.data ?: return
+                    usuarioImg.setImageURI(imageUri)
+                }
+            }
+        }
     }
 
     private fun showUserData(item: Usuario) {
